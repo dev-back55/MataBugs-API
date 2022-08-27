84e61661fbd89660ed667a51f479e48fa59d4dd2
Lucas Battaglia <lucassebastianbattaglia@gmail.com>
2022-08-27 10:53:37 -0300

    subo primeras rutas y controlador de player

--- src/controllers/players.controllers.js
+++ src/controllers/players.controllers.js
@@ -1,4 +1,55 @@
+const { Player } = require("../db.js");
+const { Op } = require("sequelize");
 
-export const PlayersPrint = (req,res)=>{
-    res.send('Players3000')
+async function getHallOfFame() {
+  let betterPlayers = await Player.findAll({ order: ["ranking", "desc"], limit : 10});
+  return betterPlayers;
 }
+
+async function searchPlayers(data) {
+  let { page, text, status, order } = data;
+  let conditions = { distinct: true, order: ["ranking", "desc"] };
+
+  size = 10;
+  page = page > 1 ? Number.parseInt(page) - 1 : 0;
+  conditions.limit = size;
+  conditions.offset = page * size;
+
+  let playerById = await getPlayerById(text);
+  if (playerById) return { players: playerById, totalPages: 1, results: 1 };
+
+  if (order) conditions.order[1] = order;
+
+  if (["oro", "plata", "bronce"].includes(status) && text)conditions.where ={[Op.and]:[{status:status},{nickname:{[Op.like]:`%${text}%`}}]}
+  else if (text) conditions.where = {[Op.or]:[{status:{[Op.like]:`%${text}%`}},{nickname:{[Op.like]:`%${text}%`}}]}
+  else if (["oro", "plata", "bronce"].includes(status)) conditions.where = {status:status}
+
+  let playersSearched = await Player.findAndCountAll(conditions);
+  playersSearched = {
+    players: playersSearched.rows,
+    totalPages: Math.ceil(playersSearched.count / size),
+    results: playersSearched.count,
+  };
+
+  return playersSearched;
+}
+
+async function createPlayer(data) {
+  let { nickname, avatar } = data;
+  await Player.create({ nickname, avatar });
+  return `the player ${nickname} was created successfully`;
+}
+
+async function updatePlayer(data) {
+  let playerUpdated = await Player.update(data, { where: { id } });
+  return `the player ${playerUpdated.dataValues.nickname} was updated successfully`;
+}
+
+async function getPlayerById(id) {
+  if (!/^[1-9][0-9]*$/.test(id)) return false;
+  let playerById = await Player.findByPk(id);
+  if (!playerById) return false;
+  return playerById;
+}
+
+module.exports = { getHallOfFame, searchPlayers, createPlayer, updatePlayer };
--- src/routes/payers.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { PlayersPrint } from './../controllers/players.controllers.js';
-
-const router = Router();
-
-router.get('/players', PlayersPrint)
-
-export default router;
