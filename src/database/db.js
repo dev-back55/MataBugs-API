747565db67b0ae2cfb736ca9b0d6430cedc5ad15
dev-back55 <ahabitu@gmail.com>
2022-08-30 14:52:56 -0300

    Feact/deployment test

--- src/database/db.js
+++ src/database/db.js
@@ -2,9 +2,9 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config()
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_DIALECT} = process.env;
 
-export const sequelize = new Sequelize('halloffame', `${DB_USER}`, `${DB_PASSWORD}`, {
+export const sequelize = new Sequelize(`${DB_DATABASE}`, `${DB_USER}`, `${DB_PASSWORD}`, {
     host: `${DB_HOST}`,
-    dialect: 'postgres'
+    dialect: `${DB_DIALECT}`
 })
