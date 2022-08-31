79c7ad587ef83edbc770e57bdbf0396c372d4063
Lucas Battaglia <lucassebastianbattaglia@gmail.com>
2022-08-31 15:15:39 -0300

    primer test de rutas arreglado

--- src/tests/playerRoutes.test.js
+++ src/tests/playerRoutes.test.js
@@ -1,17 +1,18 @@
-import supertest from "supertest";
+import request from "supertest";
 import { expect } from 'chai';
 import { sequelize } from '../database/db.js';
-import express from "express";
-const request = supertest('http://localhost:3001');
+import app from '../app.js';
+
 describe('GET inicialmente responde con un array vacío', function () {
     beforeEach(async function () {
             await sequelize.sync({ force: true })
       })
     it('GET inicialmente debe devolver un arreglo vacio',async function(){
-        const response = await request
+      const response = await request(app)
             .get("/hallOfFame")
             .set('Accept', 'application/json')
             // .expect('Content-Type', /json/) // podemos testear los headers
+          expect(response.status).to.eql(200);
           expect(response.body).to.eql([]) // testeamos la respuesta con el body
         })
     })
