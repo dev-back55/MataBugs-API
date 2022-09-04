7bc60b513695140d4de5072d374d995205696a6c
dev-back55 <ahabitu@gmail.com>
2022-09-04 14:00:54 -0300

    Feact/new scripts for deploy CD/CD AWS

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,10 +6,23 @@
 #nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+#DIR="/home/ec2-user/app"
+#if [ -d "$DIR" ]; then
+#  echo "${DIR} exists"
+#else
+#  echo "Creating ${DIR} directory"
+#  mkdir ${DIR}
+#fi
+
+echo 'run after_install.sh: ' >> /home/ec2-user/app/MataBugs-API/deploy.log
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/app >> /home/ec2-user/app/MataBugs-API/deploy.log
+
+echo 'cd /home/ec2-user/app/MataBugs-API' >> /home/ec2-user/app/MataBugs-API/deploy.log
+cd /home/ec2-user/app/MataBugs-API >> /home/ec2-user/app/MataBugs-API/deploy.log
+
+echo 'npm install' >> /home/ec2-user/app/MataBugs-API/deploy.log 
+npm install >> /home/ec2-user/app/MataBugs-API/deploy.log
+echo 'copy example to env' >> /home/ec2-user/app/MataBugs-API/deploy.log
+sudo cp .env.example .env >> /home/ec2-user/app/MataBugs-API/deploy.log
