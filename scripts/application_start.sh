91b7dfb3cd6da18b0d45747b37e9d120de88ac96
dev-back55 <ahabitu@gmail.com>
2022-09-04 11:55:07 -0300

    Feact/script ver 2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,14 +4,14 @@
 sudo chmod -R 777 /home/ec2-user/app
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/app/MataBugs-API
+cd /home/ec2-user/app
 
 sudo cp .env.example .env
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+#export NVM_DIR="$HOME/.nvm"	
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
