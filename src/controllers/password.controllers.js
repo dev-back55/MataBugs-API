24e7ec428353b234417b81428c86872103eddb25
Enzo <enz997.ing.ind@gmail.com>
2022-09-05 12:45:51 -0300

    .

--- src/controllers/password.controllers.js
+++ src/controllers/password.controllers.js
@@ -14,23 +14,24 @@ export async function recoverPassword(email){
     let userInDb = await Player.findOne({where:{email}})
     if(!userInDb)throw new Error("that user does not exist");
 
-    let token = jwt.sign({ user: userInDb }, secret, {expiresIn: expires});
+    let token = jwt.sign({ player: userInDb }, secret, {expiresIn: expires});
    
-    try {
-        const msg = {
-            to: email,
-            from: "losmatabugs@gmail.com",
-            subject:"Recover Password",
-            text:"This email was requested to change your password, please do not share with anyone, if you have not requested it, you do not need to do anything",
-            html:`<a href=${CLIENT_URL}/updatepassword/?token=${token}>Restore your password</a>`
-        }
-        await sgMail.send(msg);
-    }
-    catch(error) {
-        console.log(error)
-    }
+    // try {
+    //     const msg = {
+    //         to: email,
+    //         from: "losmatabugs@gmail.com",
+    //         subject:"Recover Password",
+    //         text:"This email was requested to change your password, please do not share with anyone, if you have not requested it, you do not need to do anything",
+    //         html:`<a href=${CLIENT_URL}/updatepassword/?token=${token}>Restore your password</a>`
+    //     }
+    //     await sgMail.send(msg);
+    // }
+    // catch(error) {
+    //     console.log(error)
+    // }
 
-    return "We have sent an email to your mailbox so that you can update your password"
+    return {msg:"We have sent an email to your mailbox so that you can update your password",
+            token: token}
 }
 
 export async function updatePassword(password, token){
@@ -45,7 +46,7 @@ export async function updatePassword(password, token){
     })
 
     password = hashSync(password, Number.parseInt(rounds));
-    await User.update({password},{where:{id : userInDb.id}})
+    await Player.update({password},{where:{id : userInDb.id}})
 
     return ("the password has been updated successfully")
 }
