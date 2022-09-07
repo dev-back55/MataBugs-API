d4363c973f9c3250b69ec7de068094bb13e80ec6
dev-back55 <ahabitu@gmail.com>
2022-09-07 11:48:03 -0300

    Feact/prueba remove Cors

--- src/app.js
+++ src/app.js
@@ -5,25 +5,25 @@ import authRoutes from './routes/auth.routes.js';
 import passwordRoutes from './routes/password.routes.js'
 import morgan from 'morgan';
 import dotenv from "dotenv";
-import cors from "cors";
+//import cors from "cors";
 
 dotenv.config()
 
 //const { CLIENT_URL } = process.env;
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 // CORS CONFIGURATION
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', CLIENT_URL);
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 //routes
 app.use(playersRoutes)
